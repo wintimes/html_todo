9fda6f0c58164a6b5719777196a4e675ebbfe26c
wintimes <842110408@qq.com>
2020-03-19 16:29:26 +0800

    javascript_studylog_2

---
+++ javascript/project/4_variable.js
@@ -0,0 +1,6 @@
+var x = 5;
+var y = 6;
+var z = x + y;
+document.write(x + "<br>");
+document.write(y + "<br>");
+document.write(z + "<br>");
---
+++ javascript/project/5_java_func.js
@@ -0,0 +1,9 @@
+function fn() {
+  document.write("1" + "</br>");
+  document.write("2" + "</br>");
+  document.write("3" + "</br>");
+  document.write("4" + "</br>");
+  document.write("5" + "</br>");
+  document.write("<hr/>");
+}
+fn();
